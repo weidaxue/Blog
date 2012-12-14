bf0cee14d302825d1e64798571eb1e4dbda09acc
songjiayang <songjiayang1@gmail.com>
2012-12-14 11:43:53 +0800

    修改了config/database.yaml

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
                    :length => { :minimum => 2 , :message =>"#标题不能少于2个字符."}
   validates :tag ,:length => { :minimum => 2 , :message =>"#关键字不能少于2个字符."}
 
-  paginates_per 4
+  paginates_per 5
 
   def all_tags
   	 tag.nil??  [] : tag.split(/,|，|\s+/)
